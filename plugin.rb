87c82f9d191102126127444e5c21c9a037cc5001
nodeloc <james@nodeloc.com>
2025-05-15 23:27:53 +0800

    csp

--- plugin.rb
+++ plugin.rb
@@ -12,33 +12,27 @@ enabled_site_setting :telegram_auth_enabled
 
 register_svg_icon "fab-telegram"
 
+extend_content_security_policy script_src: %w[https://telegram.org 'unsafe-inline']
+
 require "omniauth/telegram"
-
-after_initialize do
-  # ✅ 添加 Telegram 的 script-src 到 CSP
-  register_content_security_policy do |p|
-    p.script_src :self, "https://telegram.org", :unsafe_inline
+class ::TelegramAuthenticator < ::Auth::ManagedAuthenticator
+  def name
+    "telegram"
   end
 
-  class ::TelegramAuthenticator < ::Auth::ManagedAuthenticator
-    def name
-      "telegram"
-    end
-
-    def enabled?
-      SiteSetting.telegram_auth_enabled
-    end
-
-    def register_middleware(omniauth)
-      omniauth.provider :telegram,
-                        setup:
-                          lambda { |env|
-                            strategy = env["omniauth.strategy"]
-                            strategy.options[:bot_name] = SiteSetting.telegram_auth_bot_name
-                            strategy.options[:bot_secret] = SiteSetting.telegram_auth_bot_token
-                          }
-    end
+  def enabled?
+    SiteSetting.telegram_auth_enabled
   end
 
-  auth_provider authenticator: ::TelegramAuthenticator.new, icon: "fab-telegram"
+  def register_middleware(omniauth)
+    omniauth.provider :telegram,
+                      setup:
+                        lambda { |env|
+                          strategy = env["omniauth.strategy"]
+                          strategy.options[:bot_name] = SiteSetting.telegram_auth_bot_name
+                          strategy.options[:bot_secret] = SiteSetting.telegram_auth_bot_token
+                        }
+  end
 end
+
+auth_provider authenticator: ::TelegramAuthenticator.new, icon: "fab-telegram"
