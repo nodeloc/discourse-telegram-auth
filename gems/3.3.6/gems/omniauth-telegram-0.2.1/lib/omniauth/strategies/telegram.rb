ee79f09beae19a68ee63a292aaba0649d52026ab
nodeloc <james@nodeloc.com>
2025-05-24 06:55:07 +0800

    ifx

--- gems/3.3.6/gems/omniauth-telegram-0.2.1/lib/omniauth/strategies/telegram.rb
+++ gems/3.3.6/gems/omniauth-telegram-0.2.1/lib/omniauth/strategies/telegram.rb
@@ -1,25 +1,23 @@
-require "omniauth"
-require "openssl"
-require "base64"
+require 'omniauth'
+require 'openssl'
+require 'base64'
 
 module OmniAuth
   module Strategies
     class Telegram
       include OmniAuth::Strategy
 
-      args %i[bot_name bot_secret]
+      args [:bot_name, :bot_secret]
 
-      option :name, "telegram"
+      option :name, 'telegram'
       option :bot_name, nil
       option :bot_secret, nil
       option :button_config, {}
 
       REQUIRED_FIELDS = %w[id hash]
-      HASH_FIELDS = %w[auth_date first_name id last_name photo_url username]
+      HASH_FIELDS     = %w[auth_date first_name id last_name photo_url username]
 
       def request_phase
-        redirect_url =
-          "https://oauth.telegram.org/auth?bot_id=#{options.bot_name}&origin=#{callback_url}&embed=1"
         html = <<-HTML
           <!DOCTYPE html>
           <html>
@@ -28,16 +26,25 @@ module OmniAuth
             <title>Telegram Login</title>
           </head>
           <body>
-            <a href="#{redirect_url}" class="btn btn-primary">Login with Telegram</a> 
+        HTML
+
+        data_attrs = options.button_config.map { |k,v| "data-#{k}=\"#{v}\"" }.join(" ")
+
+        html << "<script async
+              src=\"https://telegram.org/js/telegram-widget.js?4\"
+              data-telegram-login=\"#{options.bot_name}\"
+              data-auth-url=\"#{callback_url}\"
+        #{data_attrs}></script>"
+
+        html << <<-HTML
           </body>
           </html>
         HTML
-        Rack::Response.new(html, 200, "content-type" => "text/html").finish
+
+        Rack::Response.new(html, 200, 'content-type' => 'text/html').finish
       end
 
       def callback_phase
-        Rails.logger.warn("Telegram callback params: #{request.params.inspect}")
-
         if error = check_errors
           fail!(error)
         else
@@ -45,19 +52,25 @@ module OmniAuth
         end
       end
 
-      uid { request.params["id"] }
+      uid do
+        request.params["id"]
+      end
 
       info do
         {
-          name: "#{request.params["first_name"]} #{request.params["last_name"]}",
-          nickname: request.params["username"],
-          first_name: request.params["first_name"],
-          last_name: request.params["last_name"],
-          image: request.params["photo_url"],
+            name:       "#{request.params["first_name"]} #{request.params["last_name"]}",
+            nickname:   request.params["username"],
+            first_name: request.params["first_name"],
+            last_name:  request.params["last_name"],
+            image:      request.params["photo_url"]
         }
       end
 
-      extra { { auth_date: Time.at(request.params["auth_date"].to_i) } }
+      extra do
+        {
+            auth_date: Time.at(request.params["auth_date"].to_i)
+        }
+      end
 
       private
 
@@ -72,12 +85,11 @@ module OmniAuth
       end
 
       def check_signature
-        request.params["hash"] ==
-          self.class.calculate_signature(options[:bot_secret], request.params)
+        request.params["hash"] == self.class.calculate_signature(options[:bot_secret], request.params)
       end
 
       def check_session
-        Time.now.to_i - request.params["auth_date"].to_i <= 86_400
+        Time.now.to_i - request.params["auth_date"].to_i <= 86400
       end
 
       def self.calculate_signature(secret, params)
